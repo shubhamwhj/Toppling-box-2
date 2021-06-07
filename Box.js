0aff54e20d137fcea35c283c05f4e0dce5a94517
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-06-07 10:58:49 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -5,7 +5,7 @@ class Box
         var option={
             'restitution' : 0.8
         }
-        this.body=Bodies.rectangle(x,y,width,height,option);
+        this.body=Bodies.rectangle(x,y,width,height);
         World.add(world,this.body);
         
         this.width=width;
